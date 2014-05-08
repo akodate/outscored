eef8295ab07971454776138204b9f12b923049c4
alex-zelikow <alex.zelikow@gmail.com>
2014-05-09 03:36:23 +0800

    More scraper improvement

--- .auxiliary/scraper.rb
+++ .auxiliary/scraper.rb
@@ -4,6 +4,12 @@ require 'pry'
 require 'nokogiri'
 require 'active_support/all'
 
+class Object
+  def xx
+    self.encode!('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '')
+  end
+end
+
 # SET DATA DIRECTORY AND CREATE IF NONEXISTENT
 DATA_DIR = "assets"
 Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
@@ -15,10 +21,11 @@ BASE_URL = "http://www.testprepreview.com/"
 HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
 
 # REGEXES
-CONTENT_REGEX = /((?<=addthis)(?:.*?)(?=enoch|<iframe))/im
+CONTENT_REGEX = /((?<=addthis)(?:.*)(?=enoch|<iframe))/im
 SELF_ASSESSMENT_LINKS_REGEX = /(?<=self.assessment).*/im
 ANSWER_SECTION_REGEX = /<h\d>.{,35}?(?:Answer).*?<\/h\d>.+/im
-BASE_URL_REGEX = /((?<=a\shref=")http:\/\/www.testprepreview.com\/.*?(?="))/
+BASE_URL_REGEX = /((?<=a\shref=")(?:(?:http:\/\/www.testprepreview.com\/)|(?:[^(?:http:\/\/www.)])).*?(?="))/
+EXTERNAL_URL_REGEX = /((?:http:\/\/www.))/
 
 QUESTION_REGEX = /((?:<p.{1,20}?)(?:<em>)?(?:<strong>)?.?.?\d{1,3}\.\s+.*?(?:\/p>))/m
 CHOICE_REGEX_OL = /((?:(?:<ol.{1,15}?)(?:<li>.*?<\/li>.{1,15}?))*.?<\/ol>)/m
@@ -56,23 +63,23 @@ def detect_questions(p_array, page_html)
   choices_array = []
   explanation_array = []
   p_array.each_with_index do | p, p_count |
-    if QUESTION_REGEX.match(p.to_s) && !(ANSWER_SECTION_REGEX.match(page_html.to_s).to_s.include?(p.to_s))
-      question = QUESTION_REGEX.match(p.to_s)
+    if QUESTION_REGEX.match(p.to_s.xx) && !(ANSWER_SECTION_REGEX.match(page_html.to_s.xx).to_s.xx.include?(p.to_s.xx))
+      question = QUESTION_REGEX.match(p.to_s.xx)
       question_array.push(question[0])
-      puts(p_count.to_s + " QUESTION: " + question[0])
+      puts(p_count.to_s.xx + " QUESTION: " + question[0])
     end
-    if choices = CHOICE_REGEX_P.match(p.to_s)
+    if choices = CHOICE_REGEX_P.match(p.to_s.xx)
       choices_array.push(choices[0])
-      puts(p_count.to_s + " P CHOICES: " + choices[0])
+      puts(p_count.to_s.xx + " P CHOICES: " + choices[0])
     elsif p.next_element && p.next_element.name == 'ol' && p.next_element.children.length > 2
-      choices = CHOICE_REGEX_OL.match(p.next_element.to_s)
+      choices = CHOICE_REGEX_OL.match(p.next_element.to_s.xx)
       choices_array.push(choices[0])
-      puts(p_count.to_s + " OL CHOICES: " + choices[0])
+      puts(p_count.to_s.xx + " OL CHOICES: " + choices[0])
     end
-    if EXPLANATION_REGEX.match(p.to_s) && (ANSWER_SECTION_REGEX.match(page_html.to_s).to_s.include?(p.to_s))
-      explanation = EXPLANATION_REGEX.match(p.to_s)
+    if EXPLANATION_REGEX.match(p.to_s.xx) && (ANSWER_SECTION_REGEX.match(page_html.to_s.xx).to_s.xx.include?(p.to_s.xx))
+      explanation = EXPLANATION_REGEX.match(p.to_s.xx)
       explanation_array.push(explanation[0])
-      puts(p_count.to_s + " EXPLANATION: " + explanation[0])
+      puts(p_count.to_s.xx + " EXPLANATION: " + explanation[0])
     end
   end
   question_array.length.times do | index |
@@ -84,16 +91,21 @@ end
 def detect_links(page_html, test_name, useful_urls, visited_urls)
   if a_array = page_html.css('body > table > tr > td:nth-child(2) > table > tr:nth-child(2) > td:nth-child(1) a')
 
-    a_array.each do | a |
-      binding.pry
-      if CONTENT_REGEX.match(a.to_s) && BASE_URL_REGEX.match(a.to_s)
+    a_array.each_with_index do | a, section_count |
+      if CONTENT_REGEX.match(page_html).to_s.xx.include?(p.to_s.xx) && BASE_URL_REGEX.match(a.to_s.xx)
         section_name = a.text
         section_link = a.attributes['href'].value
-        binding.pry
         if !visited_urls.include?(section_link)
 
+          visited_urls.push(section_link)
+
           begin
-            section_html = Nokogiri::HTML(open(section_link)) # OPENS TARGET PAGE
+            if !EXTERNAL_URL_REGEX.match(section_link)
+              section_html = Nokogiri::HTML(open(BASE_URL + section_link))
+            else
+              section_html = Nokogiri::HTML(open(section_link)) # OPENS TARGET PAGE
+              puts("Newly opening #{section_link} for #{section_name}")
+            end
           # RESCUE EXCEPTION
           rescue => e
             puts "Error: #{e}"
@@ -104,29 +116,36 @@ def detect_links(page_html, test_name, useful_urls, visited_urls)
           ensure
             sleep 1.0 + rand
           end
+
           json_array = detect_test(section_html)
 
-          if json_array
+          if json_array.length > 0
 
             section_dir = "#{DATA_DIR}/" + test_name + "/" + section_name
-            Dir.mkdir(section_dir) unless File.exists?(test_dir)
-            section_fname = "#{test_dir}/#{File.basename(section_name)}.html"
-            section_json = "#{test_dir}/#{File.basename(section_name)}.json"
+            Dir.mkdir(section_dir) unless File.exists?(section_dir)
+            section_fname = "#{section_dir}/#{File.basename(section_name)}.html"
+            section_json = "#{section_dir}/#{File.basename(section_name)}.json"
 
             File.open(section_fname, 'w'){|file| file.write(section_html)}
             puts "     -Saved SECTION #{section_count}: '#{section_name}' to (...#{section_fname[-20, 20]})"
 
             File.open(section_json, 'w'){|file| file.write(json_array.to_json)}
-            puts "     -Saved SECTION JSON to #{test_json}"
+            puts "     -Saved SECTION JSON to #{section_json}"
 
-            useful_urls = {section_link => {'HTML' => section_html, 'JSON' => json_array}}
+            useful_urls.update({section_link => {'HTML' => section_html, 'JSON' => json_array}})
+
+            puts "Useful URLs length updated to: #{useful_urls.length}"
 
           elsif useful_urls[section_link]
             puts "          Need to rewrite #{section_link}"
+
+          elsif json_array.length = 0
+            puts "No useful information at #{section_link}"
           end
 
         else
-          puts('#{page_link} was visited before and was useless.')
+          puts "SECTION LINK IS NULL" if section_link = nil
+          puts "#{section_link} was visited before and was useless."
         end
       end
 
@@ -139,7 +158,7 @@ end
 
 test_links.take(5).each_with_index do |a, test_count|
 
-  # next if test_count < 10
+  next if test_count < 2
 
   test_name = a.text
   test_link = a.attributes['href'].value
@@ -165,7 +184,7 @@ test_links.take(5).each_with_index do |a, test_count|
   end
 
   json_array = detect_test(test_html)
-  if json_array
+  if json_array.length > 0
     File.open(test_json, 'w'){|file| file.write(json_array.to_json)}
     puts "\t...Success, saved NEW TEST JSON to #{test_json}"
   end
@@ -255,9 +274,9 @@ end
     #       # RETURN REGEX MATCH FROM PAGE
     #       else
     #         chinese_road = CHINESE_REGEX.match(scrape_page)
-    #         puts("'" + a.text + "' is '" + chinese_road.to_s + "'")
+    #         puts("'" + a.text + "' is '" + chinese_road.to_s.xx + "'")
     #         # PUSHES ENGLISH STREET LINK & CHINESE STREET KEY/VALUE PAIR
-    #         chinese_roads.push({ a.text => chinese_road.to_s }) if chinese_road
+    #         chinese_roads.push({ a.text => chinese_road.to_s.xx }) if chinese_road
     #       # SLEEP A BIT SO THE SITE DOESN'T GET HAMMERED TOO HARD
     #       ensure
     #         sleep 1.0 + rand
